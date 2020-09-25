1c1f660065c4130d36bfa4ca9752fd27920b170c
Ayush Bansal <>
2020-09-25 12:31:06 +0530

    FE 21

--- IBM/src/index.ts
+++
@@ -1,8 +0,0 @@
-/*
- * SPDX-License-Identifier: Apache-2.0
- */
-
-import { MyAssetContract } from './my-asset-contract';
-export { MyAssetContract } from './my-asset-contract';
-
-export const contracts: any[] = [ MyAssetContract ];
